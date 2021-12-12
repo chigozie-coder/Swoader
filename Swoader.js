ca0dcc8198e3d055cccfb123c3048828b852f3a5
Chigozie Anyaeji <78688071+chigozie-coder@users.noreply.github.com>
2021-12-12 16:03:44 +0100

    Update Swoader.js

--- Swoader.js
+++ Swoader.js
@@ -63,9 +63,7 @@ class Swoader {
         this.loader.style.borderRightColor = "transparent";
         this.loader.style.animation = 'rotate 800ms linear infinite'
         this.lotext = document.getElementById('lotext');
-        this.lotext.style.fontSize = this.size/2;
         this.lotext.style.fontFamily = "courier";
-        this.lotext.style.color = "white";
         this.lotext.style.fontWeight = "bolder";
     }
     
@@ -76,9 +74,9 @@ class Swoader {
        },time)
     }
     removeWhenWindowLoaded(){
-        window.addEventListener('DOMcontentLoaded', function(){
-        setTimeout(()=>{
-            this.loadcon.style.display="none";
-        },3)
-        });
-    }
+       addEventListener("DOMContentLoaded",()=>{
+setTimeout(()=>{
+    this.loadcon.style.display="none";},3);
+});
+   }
+}
